69d3614489485e8985754f2e835b1807d825c936
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-06-26 22:34:16 -0400

    ea sports

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -55,7 +55,6 @@ import gdxforConnectionSmallIconURL from './gdxfor/gdxfor-small.svg';
 import twIcon from './tw/tw.svg';
 import turbowarpIcon from './penguinmod/extensions/turbowarp_icon.svg';
 import numberEncoding from './penguinmod/extensions/numericalencoding.png';
-import perlinNoise from './penguinmod/extensions/39f37f1b00fbf96926276701d8de4c89.png';
 import gamejolt_img from './penguinmod/extensions/gamejolt.svg';
 
 import customExtensionIcon from './custom/custom.svg';
