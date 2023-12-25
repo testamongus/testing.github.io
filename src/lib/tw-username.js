dd3f41bdee38d696c5fc6313127a912e43acd2b1
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-12-25 18:40:47 -0500

    snail

--- src/lib/tw-username.js
+++ src/lib/tw-username.js
@@ -2,7 +2,7 @@ const generateRandomUsername = () => {
     const DIGITS = 4;
     const randomNumber = Math.round(Math.random() * (10 ** DIGITS));
     const randomId = randomNumber.toString().padStart(DIGITS, '0');
-    const randomUsername = `player${randomId}`;
+    const randomUsername = `snail${randomId}`;
     return randomUsername;
 };
 
