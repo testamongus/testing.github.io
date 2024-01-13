47cb9db0627d9ebc446d71623cee1b249c846b2b
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-12 20:12:10 -0500

    oops

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -225,21 +225,21 @@ const menuItems = [
     {
         name: 'Posenet2Scratch',
         extensionId: 'posenet2scratch',
-        iconURL: 'https://raw.githubusercontent.com/champierre/facemesh2scratch/master/scratch-gui/src/lib/libraries/extensions/facemesh2scratch/facemesh2scratch.png',
-        collaborator: 'champierre',
-        internetConnectionRequired: true,
-        tags: ['other_mods'],
-        description: 'Use facetracking in your projects!',
-        featured: true
-    },
-    {
-        name: 'Facemesh2scratch',
-        extensionId: 'facemesh2scratch',
         iconURL: 'https://www.adacraft.org/studio/static/assets/cc4d68fe9d6a77c7f35f9d5461b60967.png',
         collaborator: 'champierre',
         internetConnectionRequired: true,
         tags: ['other_mods'],
         description: 'Detect human poses quickly and accurately with a normal WebCam without using a special device',
+        featured: true
+    },
+    {
+        name: 'Facemesh2scratch',
+        extensionId: 'facemesh2scratch',
+        iconURL: 'https://raw.githubusercontent.com/champierre/facemesh2scratch/master/scratch-gui/src/lib/libraries/extensions/facemesh2scratch/facemesh2scratch.png',
+        collaborator: 'champierre',
+        internetConnectionRequired: true,
+        tags: ['other_mods'],
+        description: 'Use facetracking in your projects!',
         featured: true
     },
     {
