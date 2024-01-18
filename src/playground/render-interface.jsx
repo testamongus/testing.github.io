e32ad271e386ed595025067c0c1957ac4d89ce30
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-18 18:03:51 -0500

    might fix desc and notes and credits

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -101,7 +101,7 @@ const getProjectDetailsById = async (id) => {
     // if we have already gotten the details of this project, avoid making another request since they likely never changed
     if (projectDetailCache[String(id)] != null) return projectDetailCache[String(id)];
 
-    const response = await fetch(`https://snailshare-backend.glitch.me/api/projects/getPublished?id=${id}`);
+    const response = await fetch(`https://snailshare-backend.glitch.me/api/pmWrapper/getProject?id=${id}`);
     // Don't continue if the api never returned 200-299 since we would cache an error as project details
     if (!response.ok) return {};
 
