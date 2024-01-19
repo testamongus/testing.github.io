dcf5c06dce5b4657d2699ba1609137a0c3f55121
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-19 16:01:22 -0500

    disabling this for now

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -902,22 +902,6 @@ class MenuBar extends React.Component {
                             </Button>
                         </a>
                     </div>
-                    <div className={styles.menuBarItem}>
-                        <a
-                            className={styles.feedbackLink}
-                            href="https://gemini-nine-psi.vercel.app/"
-                            rel="noopener noreferrer"
-                            target="_blank"
-                        >
-                            <Button className={styles.feedbackButton}>
-                                <FormattedMessage
-                                    defaultMessage="SnailAI [EARLY ALPHA]"
-                                    description="Button to go back to open the AI"
-                                    id="pm.aibutton"
-                                />
-                            </Button>
-                        </a>
-                    </div>
                 </div>
 
                 <div className={styles.accountInfoGroup}>
