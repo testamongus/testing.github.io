9f32345ee4f5757ffb7bca92f6511e49581b4954
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-03-02 21:50:19 -0500

    gonna fix this tomorow, revert for now

--- src/components/tw-description/description.jsx
+++ src/components/tw-description/description.jsx
@@ -1,51 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
+
 import styles from './description.css';
 import reactStringReplace from 'react-string-replace';
+
 import mdParser from 'md';
 import escape from 'scratch-vm/src/util/xml-escape';
 
 const decorate = text => {
-    // Define a regular expression to match emoji patterns
-    const emojiRegex = /:(\w+):/g;
-
-    // Replace emoji patterns with emoji images
-    text = text.replace(emojiRegex, (match, emojiName) => (
-        `<img
-            src="https://snail-ide-object-libraries.vercel.app/files/emojis/${emojiName}.png"
-            alt="${match}"
-            class="${styles.emoji}"
-        />`
-    ));
-
-    // Define a regular expression to match penguinmod.com links
-    const isPmLink = /https:\/\/penguinmod\.com\/\S*/g;
-
-    // Escape the text to prevent XSS attacks
+    // https://github.com/LLK/scratch-www/blob/25232a06bcceeaddec8fcb24fb63a44d870cf1cf/src/lib/decorate-text.jsx
+    
+    const isPmLink = /https:\/(\/\w+\.|\/)penguinmod\.(site\/.*|site)/sg;
     const escaped = escape(text);
-
-    // Parse Markdown and ensure links open in new tab
     const htmlText = mdParser(escaped, {
         linksInNewTab: link => isPmLink.test(link)
     });
+    // not disabling this just incase, but this is intentional
+    const html = (<div dangerouslySetInnerHTML={{__html: htmlText}} />);
 
     // Make links clickable
     const linkRegex = /(https?:\/\/[\w\d_\-.]{1,256}(?:\/(?:\S*[\w:/#[\]@$&'()*+=])?)?(?![^?!,:;\w\s]\S))/g;
-    text = reactStringReplace(htmlText, linkRegex, match => (
-        `<a href="${match}" rel="noreferrer">${match}</a>`
+    text = reactStringReplace(html, linkRegex, (match, i) => (
+        <a
+            href={match}
+            rel="noreferrer"
+            key={match + i}
+        >{match}</a>
     ));
 
-    return { __html: text };
-};
+    // Make hashtags clickable
+    text = reactStringReplace(text, /#([\w-]+)/g, (match, i) => (
+        <a
+            href={`https://snail-ide.js.org/#${match}`}
+            key={match + i}
+        >{`#${match}`}</a>
+    ));
 
+    // Make @s clickable
+    text = reactStringReplace(text, /@([\w-]+)/g, (match, i) => (
+        <a
+            href={`https://scratch.mit.edu/users/${match}`}
+            key={match + i}
+        >{`#${match}`}</a>
+    ));
+
+    return text;
+};
 
 const Description = ({
     instructions,
     credits
 }) => instructions !== 'unshared' && credits !== 'unshared' && (
     <div className={styles.description}>
-        {instructions && (
+        {instructions ? (
             <div>
                 <h2 className={styles.header}>
                     <FormattedMessage
@@ -54,13 +62,12 @@ const Description = ({
                         id="tw.home.instructions"
                     />
                 </h2>
-                <div dangerouslySetInnerHTML={decorate(instructions)} />
-
+                {decorate(instructions)}
             </div>
-        )}
-        {instructions && credits && (
+        ) : null}
+        {instructions && credits ? (
             <div className={styles.divider} />
-        )}
+        ) : null}
         {credits && (
             <div>
                 <h2 className={styles.header}>
@@ -70,8 +77,7 @@ const Description = ({
                         id="tw.home.credit"
                     />
                 </h2>
-                <div dangerouslySetInnerHTML={decorate(credits)} />
-
+                {decorate(credits)}
             </div>
         )}
     </div>
