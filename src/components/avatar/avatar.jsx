8d700acb1f95640691417e6467bab383f2556cb5
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-03-05 19:00:27 -0500

    we should make this use the api to fetch avatar pictures

---
+++ src/components/avatar/avatar.jsx
@@ -0,0 +1,22 @@
+const user = {
+    name: 'Me_rudy',
+    imageUrl: 'https://trampoline.turbowarp.org/avatars/by-username/Mr_rudy',
+    imageSize: 90,
+  };
+  
+  export default function Profile() {
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Avatar of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    );
+  }
