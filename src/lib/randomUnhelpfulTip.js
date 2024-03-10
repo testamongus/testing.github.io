097f098ce36c19f86b09d86c7e9ee93f01d86091
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-03-09 19:28:47 -0500

    Update randomUnhelpfulTip.js

--- src/lib/randomUnhelpfulTip.js
+++ src/lib/randomUnhelpfulTip.js
@@ -4,4 +4,16 @@ export const tip = [
     'Also check out LibreKitten!',
     'Snail IDE is based off the source code of PenguinMod, which is based off TurboWarp, which is based off Scratch. M O D C E P T I O N.',
     'Crazy? I was crazy once. They locked me in a room, a rubber room. A rubber room with rats. And rats make me crazy.',
+    "Snail IDE.",
+    "I LOVE PENGUINMOD!!!",
+    "In 2089, literally nothing will happen.",
+    "Did you know? You can post your projects outside of the Snail IDE community.",
+    "number 15 burger king foot lettuce",
+    "thepython555 the goat",
+    "Snails",
+    "penguinmod:menguinpod, snail-ide:ide-snail",
+    "If the creator of Snail IDE (Mr_rudy) enjoys your project, he will give you the COOL badge.",
+    "The first project to be uploaded that still exists is SnailGPT by Mr_rudy, keep in mind this lasted through over 3 server changes",
+    "Snail IDE Noticed ScratchDaPony :)",
+    "@redspacecat is remaking Geometry Dash in Snail IDE, with level sharing!"
 ];
